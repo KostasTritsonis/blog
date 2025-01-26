774bd9abdfefff679c87c9283424d5455b9bc90a
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-26 20:25:53 +0200

    26012025

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { prisma } from "@/lib/db"
 import { notFound } from "next/navigation";
 
-export default async function Page({params}: {params: {id: string}}) {
+export default async function Page({params}: {params: Promise<{ id: string }>}) {
 
+  const { id } = await params;
   const post = await prisma.post.findUnique({
     where: {
-      id: Number(params.id)
+      id: Number(id)
     }
   });
 
