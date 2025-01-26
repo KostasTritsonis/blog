8c0a38c23962ac13f26f8100a7654be0b04baf66
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-26 22:01:00 +0200

    26012025

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
   return (
     <main className="text-center pt-16">
         <h1 className="text-5xl font-semibold mb-7">Create Post</h1>
-        <form className='flex flex-col mx-auto max-w-[400px] gap-2 my-10' action={createPost}>
+        <form className='flex flex-col mx-auto max-w-[350px] gap-2 my-10' action={createPost}>
             <input className='border rounded px=3 h-10 py-2' type="text" name="title" placeholder='Title for new post' required/>
             <textarea className='border rounded px=3' name="body" rows={6} placeholder='Content for new post' required></textarea>
             <button className='h-10 bg-blue-500 px-5 rounded text-white'>Submit</button>
