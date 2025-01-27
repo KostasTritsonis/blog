a2958b67ff64f2426e6a3d5687efab36d320283d
KostasTritsonis <ktritsonis@hotmail.com>
2025-01-27 14:36:07 +0200

    27012025

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -1,18 +1,19 @@
 import { prisma } from '@/lib/db';
 import Link from 'next/link'
+import { Suspense } from 'react';
 
 export default async function PostsList() {
 
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   const posts = await prisma.post.findMany();
 
   return (
     <>
+     <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts({posts.length})</h1>
      {posts.length === 0 ? (
         // Display a message if there are no posts
         <p className="text-gray-500">There are no posts.</p>
       ) : (
-        <ul>
+        <ul className='border-t border-black/10 py-5 max-w-[750px] max-sm:max-w-[350px] mx-auto'>
           {posts.map((post) => (
             <li key={post.id} className='mb-3'>
               <Link href={`/posts/${post.id}`}>{post.title}</Link>
